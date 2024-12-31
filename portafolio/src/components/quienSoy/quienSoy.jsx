385a6f86ae53c5ce1769edccd8784ff1117d55b8
Hector Vera <hveramtz.inf@gmail.com>
2024-12-30 23:12:48 -0300

    arreglando diseños web otra vez 😞  ahora adaptativos con vh 😄

--- portafolio/src/components/quienSoy/quienSoy.jsx
+++ portafolio/src/components/quienSoy/quienSoy.jsx
@@ -9,7 +9,7 @@ function QuienSoy() {
     return (
         <div className='quien-soy'>
             <Container sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 2, gap: 2 }}>
-                <Card className="card-hover" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '300px', backgroundColor: '#aba8a8', border:'1px solid #060202', borderRadius:'20px' }}>
+                <Card className="card-hover" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '40vh', backgroundColor: '#aba8a8', border:'1px solid #060202', borderRadius:'20px' }}>
                     <Avatar src={imgPerfil} alt="Foto de perfil" sx={{ width: 100, height: 100 }} />
                     <CardHeader title="Hector Vera Martinez" subheader="Ingeniero de Ejecucion en Informatica" />
                     <CardContent sx={{ padding: '0px !important' }}>
@@ -39,12 +39,9 @@ function QuienSoy() {
                     </CardContent>
                 </Card>
                 <Grow in={true}>
-                    <Card className="card-hover" sx={{ padding: 1 , width:'500px', height:'auto', backgroundColor: '#aba8a8', border:'1px solid #060202', borderRadius:'20px' }}>
+                    <Card className="card-hover" sx={{ padding: 1 , width:'70vh', height:'auto', backgroundColor: '#aba8a8', border:'1px solid #060202', borderRadius:'20px' }}>
                         <CardContent sx={{paddingBottom:'16px !important'}}>
-                            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-                                <p className='parrafo'>Para mí, el desarrollo de software no solo se trata de escribir código; se trata de resolver problemas reales. Mi filosofía se centra en comprender a fondo las necesidades de los usuarios y diseñar soluciones tecnológicas que no solo sean funcionales, sino que también brinden valor y mejoren la vida de las personas.</p>
-                                <p className='parrafo'>Me apasiona el desarrollo web y móvil, y disfruto trabajando con tecnologías modernas y herramientas de vanguardia. Me considero un aprendiz constante y siempre estoy buscando mejorar mis habilidades y conocimientos.</p>
-                            </Box>
+                            <p className='parrafo'>Para mí, el desarrollo de software no solo se trata de escribir código; se trata de resolver problemas reales. Mi filosofía se centra en comprender a fondo las necesidades de los usuarios y diseñar soluciones tecnológicas que no solo sean funcionales, sino que también brinden valor y mejoren la vida de las personas. Me apasiona el desarrollo web y móvil, y disfruto trabajando con tecnologías modernas y herramientas de vanguardia. Me considero un aprendiz constante y siempre estoy buscando mejorar mis habilidades y conocimientos.</p>
                         </CardContent>
                     </Card>
                 </Grow>
