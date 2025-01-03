65ce72375836d1250f38bdacf7bde839ae9876b9
Hector Vera <hveramtz.inf@gmail.com>
2025-01-03 11:20:39 -0300

    speed y analystic

--- portafolio/src/main.jsx
+++ portafolio/src/main.jsx
@@ -1,17 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import './index.css';
 import App from './App.jsx';
 
 function Main() {
+
+  const vercelToken = import.meta.env.VITE_VERCEL_TOKEN;
+  const vercelProjectId = import.meta.env.VITE_VERCEL_PROJECT_ID;
+
   return (
     <StrictMode>
-      <SpeedInsights>
-        <div className="app-content transition">
-          <App />
-        </div>
-      </SpeedInsights>
+      <div className="app-content transition">
+        <App />
+      </div>
+      <SpeedInsights token={vercelToken} projectId={vercelProjectId}/>
+      <Analytics/>
     </StrictMode>
   );
 }
