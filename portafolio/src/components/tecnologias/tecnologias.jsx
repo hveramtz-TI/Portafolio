bc2cf63c63177eafabcb8e31fae0cdf1158b464b
Hector Vera <hveramtz.inf@gmail.com>
2025-01-07 15:30:40 -0300

    Update tecnologias.jsx

--- portafolio/src/components/tecnologias/tecnologias.jsx
+++ portafolio/src/components/tecnologias/tecnologias.jsx
@@ -8,68 +8,55 @@ function Tecnologias() {
   useEffect(() => {
     const carousel = carouselRef.current;
 
-    let interval;
+    let animationFrame;
     let isScrolling = false;
 
     const scrollStep = () => {
       if (carousel && !isScrolling) {
-        const speed = window.innerWidth <= 768 ? 4 : 2; // Velocidad móvil/desktop
+        const speed = window.innerWidth <= 768 ? 2 : 2; // Velocidad móvil/desktop
         carousel.scrollLeft += speed;
-        if (carousel.scrollLeft + carousel.clientWidth >= carousel.scrollWidth) {
+        if (carousel.scrollLeft + carousel.offsetWidth >= carousel.scrollWidth) {
           carousel.scrollLeft = 0;
         }
+        animationFrame = requestAnimationFrame(scrollStep);
       }
     };
 
     const startScroll = () => {
-      clearInterval(interval);
-      interval = setInterval(scrollStep, 16); // Más suave (16ms ≈ 60FPS)
+      cancelAnimationFrame(animationFrame);
+      animationFrame = requestAnimationFrame(scrollStep);
     };
 
     const stopScroll = () => {
-      clearInterval(interval);
+      cancelAnimationFrame(animationFrame);
       isScrolling = true;
       setTimeout(() => {
         isScrolling = false;
-        startScroll();
-      }, 3000); // Pausa 3 segundos tras interacción
+      }, 200);
     };
 
-    // Eventos de interacción
-    carousel.addEventListener('pointerdown', stopScroll);
-    carousel.addEventListener('pointerup', startScroll);
+    // Agrega eventos
+    carousel.addEventListener('mouseenter', stopScroll);
+    carousel.addEventListener('mouseleave', startScroll);
     carousel.addEventListener('touchstart', stopScroll);
     carousel.addEventListener('touchend', startScroll);
 
-    startScroll(); // Inicia el scroll al montar
+    startScroll();
 
     return () => {
-      clearInterval(interval);
-      if (carousel) {
-        carousel.removeEventListener('pointerdown', stopScroll);
-        carousel.removeEventListener('pointerup', startScroll);
-        carousel.removeEventListener('touchstart', stopScroll);
-        carousel.removeEventListener('touchend', startScroll);
-      }
+      cancelAnimationFrame(animationFrame);
+      carousel.removeEventListener('mouseenter', stopScroll);
+      carousel.removeEventListener('mouseleave', startScroll);
+      carousel.removeEventListener('touchstart', stopScroll);
+      carousel.removeEventListener('touchend', startScroll);
     };
   }, []);
 
   return (
     <div className="carousel" ref={carouselRef}>
-      {tecnologias.concat(tecnologias).map((tecnologia, index) => (
+      {tecnologias.map((tecnologia, index) => (
         <div key={index} className="carousel-item">
-          <a href={tecnologia.link} target="_blank" rel="noopener noreferrer">
-            <img
-              src={tecnologia.icono}
-              alt={tecnologia.nombre}
-              style={{
-                cursor: 'pointer',
-                width: '8vh',
-                height: '8vh',
-                objectFit: 'contain',
-              }}
-            />
-          </a>
+          <img className="carousel-img" src={tecnologia.icono} alt={tecnologia.nombre} />
         </div>
       ))}
     </div>
